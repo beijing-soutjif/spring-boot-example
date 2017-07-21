4618c909051b9ee87337b7bc267e0a27801e7ff4
chaoj <chaojing5168@gmai.com>
2017-07-21 14:35:13 +0800

    注释添加

--- src/main/java/com/example/springboot/DemoPublishApplication.java
+++ src/main/java/com/example/springboot/DemoPublishApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-/*
+ 
+/**
+ * @author chaoj
  * 打包发表到tomcat
  */
 @SpringBootApplication
--- src/main/java/com/example/springboot/DemoTestApplication.java
+++ src/main/java/com/example/springboot/DemoTestApplication.java
@@ -3,7 +3,8 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
+/**
+ * @author chaoj
  * 测试用
  */
 @SpringBootApplication
