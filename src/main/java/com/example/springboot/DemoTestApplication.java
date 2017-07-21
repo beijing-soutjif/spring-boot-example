93a7e72680e9a1902a2663bdb1a242201135ee38
chaoj <chaojing5168@gmai.com>
2017-07-21 17:19:36 +0800

    代码提交测试4

--- src/main/java/com/example/springboot/DemoTestApplication.java
+++ src/main/java/com/example/springboot/DemoTestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
- * 测试用
+ * 测试启动类DemoTestApplication
  */
 @SpringBootApplication
 public class DemoTestApplication {
