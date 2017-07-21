01782e3e1fb84edd5dd03a206d8b2d6e682b535c
chaoj <chaojing5168@gmai.com>
2017-07-21 16:43:08 +0800

    格式化代码

--- src/main/java/com/example/springboot/User.java
+++ src/main/java/com/example/springboot/User.java
@@ -1,25 +1,27 @@
 package com.example.springboot;
 
 /**
- * @author chaoj
- * 用户user实体类
+ * @author chaoj 用户user实体类
  */
 public class User {
 
 	private String name;
 	private String pwd;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPwd() {
 		return pwd;
 	}
+
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
-	
-	
+
 }
