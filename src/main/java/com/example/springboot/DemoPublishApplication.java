f62d72c3e35bf5e8900dc8c041f9d96ce0abaf2f
chaoj <chaojing5168@gmai.com>
2017-07-21 14:03:25 +0800

    spring-boot测试实例

---
+++ src/main/java/com/example/springboot/DemoPublishApplication.java
@@ -0,0 +1,24 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/*
+ * 打包发表到tomcat
+ */
+@SpringBootApplication
+public class DemoPublishApplication extends SpringBootServletInitializer {
+     
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(DemoPublishApplication.class);
+    }
+ 
+     
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPublishApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/springboot/DemoTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * 测试用
+ */
+@SpringBootApplication
+public class DemoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestApplication.class, args);
+	}
+}
